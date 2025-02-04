3b6dac14a3442b8eb73db57559c5a6a699b7e415
Kabir Pakhale <115333903+K4bir4@users.noreply.github.com>
2025-02-04 22:32:02 +0530

    Create 4.py

---
+++ notes and codes/practice coding/matplotlib/4.py
@@ -0,0 +1,10 @@
+###Bar Plor
+categories=['A','B','C','D','E']
+values=[5,7,3,8,6]
+
+##create a bar plot
+plt.bar(categories,values,color='purple')
+plt.xlabel('Categories')
+plt.ylabel('Values')
+plt.title('Bar Plot')
+plt.show()
