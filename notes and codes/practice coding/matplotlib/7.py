d7afefdbeeff6a9745e46127c50d0787e1193835
Kabir Pakhale <115333903+K4bir4@users.noreply.github.com>
2025-02-05 23:28:02 +0530

    Update 7.py

--- notes and codes/practice coding/matplotlib/7.py
+++ notes and codes/practice coding/matplotlib/7.py
@@ -6,4 +6,4 @@ colors=['gold','yellowgreen','lightcoral','lightskyblue']
 explode=(0.2,0,0,0) ##move out the 1st slice
 
 ##create apie chart
-plt.pie(sizes,explode=explode,labels=labels,colors=colors,autopct="%1.1f%%",shadow=True)7.py
+plt.pie(sizes,explode=explode,labels=labels,colors=colors,autopct="%1.1f%%",shadow=True)
