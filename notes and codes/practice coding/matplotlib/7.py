450ffd1a6bcdf3bce8dcb3ffdadf1b40430592be
Kabir Pakhale <115333903+K4bir4@users.noreply.github.com>
2025-02-05 23:27:51 +0530

    Create 7.py

---
+++ notes and codes/practice coding/matplotlib/7.py
@@ -0,0 +1,9 @@
+### pie chart
+
+labels=['A','B','C','D']
+sizes=[30,20,40,10]
+colors=['gold','yellowgreen','lightcoral','lightskyblue']
+explode=(0.2,0,0,0) ##move out the 1st slice
+
+##create apie chart
+plt.pie(sizes,explode=explode,labels=labels,colors=colors,autopct="%1.1f%%",shadow=True)7.py
